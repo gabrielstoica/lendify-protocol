ca432df23de1eadae0cc9f07165b3fdf4b913caa
gabrielstoica <marius_gabriel1998@yahoo.com>
2024-02-09 10:49:19 +0200

    feat: add 'Tokens' component and display the list

--- src/components/Tokens.tsx
+++ src/components/Tokens.tsx
@@ -1,48 +1,32 @@
-import { View } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "./ui/button";
 
-export default function Tokens({ assets }: any) {
-  console.log(assets);
-  return (
-    <div className="max-w-2xl px-4 py-16 sm:px-6 sm:py-12 lg:max-w-7xl lg:px-8">
-      <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-        {assets.map((asset: any) => (
-          <div key={asset.id} className="group relative">
-            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-              <Image
-                src={asset.image_url}
-                alt={asset.name}
-                className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                width={500}
-                height={500}
-              />
-            </div>
-            <div className="mt-4 flex justify-between">
-              <div>
-                <Link href={`/dashboard/memory/${asset.token_id}`}>
-                  <h3 className="text-lg font-medium text-gray-900">
-                    <span aria-hidden="true" className="absolute inset-0" />
-                    {asset.name}
-                  </h3>
-                </Link>
-                <p className="mt-1 text-sm text-gray-500">
-                  {new Intl.DateTimeFormat("en-Gb", {
-                    year: "numeric",
-                    month: "2-digit",
-                    day: "2-digit",
-                    hour: "2-digit",
-                    minute: "2-digit",
-                  }).format(new Date(asset.created_at))}
-                </p>
-              </div>
-              <p className="text-sm font-medium text-gray-900">
-                <View color="grey" />
-              </p>
-            </div>
-          </div>
-        ))}
+export default function Tokens({ tokens }: any) {
+  return tokens.map((token: any) => (
+    <div key={token.id} className="token ml-2">
+      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+        <Image
+          src={token.image}
+          alt={token.name}
+          className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+          width={500}
+          height={500}
+        />
+      </div>
+      <div className="mt-4 flex justify-between">
+        <div>
+          <Link href={`/dashboard/memory/${token.token_id}`}>
+            <h3 className="text-lg font-medium text-gray-900">
+              <span aria-hidden="true" className="absolute inset-0" />
+              {token.name}
+            </h3>
+          </Link>
+        </div>
+        <p className="text-sm font-medium text-gray-900">
+          <Button size="sm">Lend</Button>
+        </p>
       </div>
     </div>
-  );
+  ));
 }

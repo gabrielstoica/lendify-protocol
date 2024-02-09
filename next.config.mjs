f3799c0ffd5fce211904924a5caf55a3e34b2fb6
gabrielstoica <marius_gabriel1998@yahoo.com>
2024-02-09 10:51:26 +0200

    fix: add the Infura IPFS dedicated gateway as a trustued hostname

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "reneum.infura-ipfs.io",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
